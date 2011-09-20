85305ad4918a481538b84adbb76545f0bc81b778
Paul Connolley <shunuk@gmail.com>
2011-09-20 13:58:51 +0100

    Added method fuzzyize to create fuzzy regex patterns

--- models/search_index.php
+++ models/search_index.php
@@ -1,65 +1,78 @@
 <?php
 class SearchIndex extends AppModel {
-	var $name = 'SearchIndex';
-	private $models = array();
+    var $name = 'SearchIndex';
+    private $models = array();
 
-	private function bindTo($model) {
-		$this->bindModel( 
-			array(
-				'belongsTo' => array(
-					$model => array (
-						'className' => $model,
-						'conditions' => 'SearchIndex.model = \''.$model.'\'',
-						'foreignKey' => 'association_key'
-					)
-				)
-			),false 
-		);
-	}
-	
-	
-	function searchModels($models = array()) {
-		if (is_string($models)) $models = array($models);
-		$this->models = $models;
-		foreach ($models as $model) {
-			$this->bindTo($model);
-		}
-	}
-		
-	function beforeFind($queryData) {
-		$models_condition = false;
-		if (!empty($this->models)) {
-			$models_condition = array();
-			foreach ($this->models as $model) {
-				$Model = ClassRegistry::init($model);
-				$models_condition[] = $model . '.'.$Model->primaryKey.' IS NOT NULL'; 
-			}
-		}
-		
-		if (isset($queryData['conditions'])) {
-			if ($models_condition) {
-				if (is_string($queryData['conditions'])) {
-					$queryData['conditions'] .= ' AND (' . join(' OR ',$models_condition) . ')';
-				} else {
-					$queryData['conditions'][] = array('OR' => $models_condition);
-				}
-			}
-		} else {
-			if ($models_condition) {
-				$queryData['conditions'][] = array('OR' => $models_condition);
-			}
-		}
-		return $queryData; 	
-	}
+    private function bindTo($model) {
+        $this->bindModel( 
+            array(
+                'belongsTo' => array(
+                    $model => array (
+                        'className' => $model,
+                        'conditions' => 'SearchIndex.model = \''.$model.'\'',
+                        'foreignKey' => 'association_key'
+                    )
+                )
+            ),false 
+        );
+    }
 
-	function afterFind($results, $primary) {
-		foreach($results as $x => $result) {
-			if (!empty($result['SearchIndex'])) {
-				$Model = ClassRegistry::init($result['SearchIndex']['model']);
-				$results[$x]['SearchIndex']['displayField'] = $Model->displayField;
-			}
-		}
-		return $results;
-	}
+    function searchModels($models = array()) {
+        if (is_string($models)) $models = array($models);
+        $this->models = $models;
+        foreach ($models as $model) {
+            $this->bindTo($model);
+        }
+    }
+
+    function beforeFind($queryData) {
+        $models_condition = false;
+        if (!empty($this->models)) {
+            $models_condition = array();
+            foreach ($this->models as $model) {
+                $Model = ClassRegistry::init($model);
+                $models_condition[] = $model . '.'.$Model->primaryKey.' IS NOT NULL'; 
+            }
+        }
+
+        if (isset($queryData['conditions'])) {
+            if ($models_condition) {
+                if (is_string($queryData['conditions'])) {
+                    $queryData['conditions'] .= ' AND (' . join(' OR ',$models_condition) . ')';
+                } else {
+                    $queryData['conditions'][] = array('OR' => $models_condition);
+                }
+            }
+        } else {
+            if ($models_condition) {
+                $queryData['conditions'][] = array('OR' => $models_condition);
+            }
+        }
+        return $queryData;  
+    }
+
+    function afterFind($results, $primary) {
+        foreach($results as $x => $result) {
+            if (!empty($result['SearchIndex'])) {
+                $Model = ClassRegistry::init($result['SearchIndex']['model']);
+                $results[$x]['SearchIndex']['displayField'] = $Model->displayField;
+            }
+        }
+        return $results;
+    }
+
+    function fuzzyize($query) {
+        $query = preg_replace('/\s+/i', ' ', $query);
+        $fuzzies = str_split($query);
+        foreach ($fuzzies as $i => $fuzz) {
+            $fuzzies[$i] = preg_quote($fuzz);
+            if ($fuzz == ' ') {
+                $fuzzies[$i] = '[[:blank:]]*';
+            } else {
+                $fuzzies[$i] = $fuzz . '[[:alnum:]]*';
+            }
+        }
+        $query = join('', $fuzzies);
+        return $query;
+    }
 }
-?>
